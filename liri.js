373f173b0bab2a378964711e8c26ec5b4c057999
Matt Pignatore <mattpigs@gmail.com>
2019-10-24 21:10:25 -0400

    added the do-what-it-says function that reads the txt file

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 var fs = require('fs');
 var keys = require("./keys.js");
 var axios = require('axios');
@@ -8,25 +7,25 @@ var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 var searchType = process.argv[2];
-var userSearch = process.argv[3];
+var userSearch = process.argv.slice(3).join(' ');
 
 switch (searchType) {
     case 'concert-this':
-        getTourDates();
+        getTourDates(userSearch);
     break;
     case 'spotify-this-song':
-        getThisSong();
+        getThisSong(userSearch);
     break;
     case 'movie-this':
-        getMovies();
+        getMovies(userSearch);
     break;
     case 'do-what-it-says':
-
+        doThis();
     break;
 }
 
-function getTourDates() {
-    var bandsInTownURL = 'https://rest.bandsintown.com/artists/' + userSearch + '/events?app_id=codingbootcamp';
+function getTourDates(band) {
+    var bandsInTownURL = 'https://rest.bandsintown.com/artists/' + band + '/events?app_id=codingbootcamp';
     axios.get(bandsInTownURL).then(function(response) {
         var tourInfo = response.data;
         for (var i = 0; i < tourInfo.length; i++) {
@@ -37,49 +36,67 @@ function getTourDates() {
             var state = tourInfo[i].venue.region;
             var country = tourInfo[i].venue.country;
             if (state === '') {
-                console.log(date + ' at ' + time + ' - ' + venue + ' in ' + city + ', ' + country);
+                console.log(`${date} at ${time} - ${venue} in ${city}, ${country}`);
             } else {
-                console.log(date + ' at ' + time + ' - ' + venue + ' in ' + city + ', ' + state);
+                console.log(`${date} at ${time} - ${venue} in ${city}, ${state}`);
             }
         }
     })
 }
 
-function getMovies() {
-    var omdbURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + userSearch;
+function getMovies(movie) {
+    var omdbURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie;
     axios.get(omdbURL).then(function(response) {
         var movieInfo = response.data;
-        var title = movieInfo.Title;
-        var releaseYear = movieInfo.Year;
-        var imdbRating = movieInfo.imdbRating;
-        var rotten = movieInfo.Ratings[1].Source;
-        var rottenRating = movieInfo.Ratings[1].Value;   
-        var country = movieInfo.Country;
-        var lang = movieInfo.Language;
-        var plot = movieInfo.Plot;
-        var actors = movieInfo.Actors;
-        console.log(title);
-        console.log('Released in ' + releaseYear);
-        console.log('IMDB rating of ' + imdbRating);
-        console.log(rotten + ' Rating: ' + rottenRating);
-        console.log('Produced in ' + country);
-        console.log('Language: ' + lang);
-        console.log('Plot summary: ' + plot);
-        console.log('Cast: ' + actors);
+        console.log(movieInfo.Title);
+        console.log('Released in ' + movieInfo.Year);
+        console.log('IMDB rating of ' + movieInfo.imdbRating);
+        console.log(movieInfo.Ratings[1].Source + ' Rating: ' + movieInfo.Ratings[1].Value);
+        console.log('Produced in ' + movieInfo.Country);
+        console.log('Language: ' + movieInfo.Language);
+        console.log('Plot summary: ' + movieInfo.Plot);
+        console.log('Cast: ' + movieInfo.Actors);
     })
 }
 
-function getThisSong() {
+function getThisSong(song) {
     spotify.search({ 
         type: 'track', 
-        query: userSearch 
+        query: song 
     }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-      console.log("Artist: " + data.tracks.items[0].artists[0].name); 
-      console.log("Song Name: " + data.tracks.items[0].name); 
-      console.log("Preview: " + data.tracks.items[0].preview_url); 
-      console.log("Album: " + data.tracks.items[0].album.name); 
+        var spotifyReturn = data.tracks.items;
+        for (var i = 0; i < spotifyReturn.length; i++) {
+            console.log("Artist: " + data.tracks.items[i].artists[0].name); 
+            console.log("Song Name: " + data.tracks.items[i].name); 
+            console.log("Preview: " + data.tracks.items[i].preview_url); 
+            console.log("Album: " +data.tracks.items[i].album.name); 
+            console.log('-----------------------------------------------------');
+        }
       });
+}
+
+function doThis() {
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+          }
+        var newCommand = data.split(' ');
+        var newSearch = newCommand[0];
+        var newInput = newCommand.slice(1).join(' ');
+
+        switch(newSearch) {
+            case 'concert-this':
+                getTourDates(newInput);
+            break;
+            case 'spotify-this-song':
+                getThisSong(newInput);
+            break;
+            case 'movie-this':
+                getMovies(newInput);
+            break;
+        }
+    })
 }
